6f072c2ef90acb68f5013a52d15899610ad6a841
KangkanJyoti <kangkan.bora@reverieinc.com>
2015-10-12 02:42:40 +0530

    First Commit

---
+++ testJar.js
@@ -0,0 +1,13 @@
+var exec = require('child_process').exec, child;
+child = exec('java -jar Test1.jar',
+        
+  function (error, stdout, stderr){
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if(error !== null){
+      console.log('exec error: ' + error);
+    }
+});
+
+
+//java -jar webapp-runner.jar application.war
